9facf40e70a13a603d8c46367b259728ace3b89e
Abadi Suryo <abadisuryosetiyo@gmail.com>
2024-04-30 23:02:38 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/eternalenterprise/aidafine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eternalenterprise.aidafine.aidafine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:aidafine/app/app.dart';
+import 'package:aidafine/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
